dffec86e239c94fa63fde7ee1b30a763ce48cf17
koles <76480929+xkolesx@users.noreply.github.com>
2023-01-31 22:16:51 +0100

    Update TrafficSpawnerAndDestroyer.cs

--- Crazy Delivery/Assets/Scripts/EnvironmentScripts/TrafficSpawnerAndDestroyer.cs
+++ Crazy Delivery/Assets/Scripts/EnvironmentScripts/TrafficSpawnerAndDestroyer.cs
@@ -9,21 +9,23 @@ namespace EnvironmentScripts
     {
         [SerializeField] private List<GameObject> _trafficPreffabs;
         [SerializeField] private float _spawningDelay;
-        [SerializeField] private float _speedOfCarRiding;
+        [SerializeField] private float _speedOfCarRiding; // переименовить carSpeed
         
         private bool _canSpawn = true;
-        private Vector3 _spawnPos;
+        private Vector3 _spawnPos;// position
         private Quaternion _spawnRotation;
     
-        public float SpeedOfCarRidingForCarRiding { get; private set; }
+        public float SpeedOfCarRidingForCarRiding { get; private set; } // исправить на то что ниже
+        
+        public float SpeedOfCarRidingForCarRiding => _speedOfCarRiding;    
         
         private void Start()
         {
-            SpeedOfCarRidingForCarRiding = _speedOfCarRiding;
+            //SpeedOfCarRidingForCarRiding = _speedOfCarRiding;
             _spawnPos = transform.position;
             if (transform.position.z > 0)
             {
-                _spawnPos.z -= 20;
+                _spawnPos.z -= 20; // убрать магическое число
             }
             else
             {
@@ -48,19 +50,26 @@ namespace EnvironmentScripts
             }
         }
         
+        private void CreateCar(Quaternion rotation)
+        {
+            Instantiate(_trafficPreffabs[Random.Range(0, _trafficPreffabs.Count - 1)], _spawnPos, rotation, transform);
+        }
+        
         private IEnumerator SpawningDelay()
         {
             yield return new WaitForSeconds(_spawningDelay);
             if (transform.position.z > 0)
             {
-                Instantiate(_trafficPreffabs[Random.Range(0, _trafficPreffabs.Count - 1)], _spawnPos, Quaternion.Euler(0f, 180f, 0f), transform);
+            CreateCar(Quaternion.Euler(0f, 180f, 0f));
+               
             }
             else
             {
-                Instantiate(_trafficPreffabs[Random.Range(0, _trafficPreffabs.Count - 1)], _spawnPos, Quaternion.Euler(0f, 0f, 0f), transform);
+             CreateCar(Quaternion.Euler(0f, 0f, 0f));
             }
             
             _canSpawn = true;
         }
+       
     }
 }
